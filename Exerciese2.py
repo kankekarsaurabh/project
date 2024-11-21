b018de6ed74033ef1ae14d34ae26d8eaed41c7d8
kankekarsaurabh <kankekarsaurabh98@gmail.com>
2024-11-21 12:07:53 -0800

    exercise2

---
+++ Exerciese2.py
@@ -0,0 +1,16 @@
+
+#2
+heros=['spider man','thor','hulk','iron man','captain america']
+
+heros.append("black panther")
+print(heros)
+
+heros.remove("black panther")
+print(heros)
+
+heros.insert(3,"black panther")
+print(heros)
+
+
+heros[1:3]=["Doctor Strange"]
+print(heros)
