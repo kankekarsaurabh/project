c3aba63255048c90379ddecacbace2fe517cb8b4
kankekarsaurabh <kankekarsaurabh98@gmail.com>
2024-11-21 12:34:46 -0800

    odd numbers

--- Exerciese2.py
+++ Exerciese2.py
@@ -1,19 +1,15 @@
+#Create a list of all odd numbers between 1 and a max number. 
+# Max number is something you need to take from a user using input() function
 
-#2
-heros=['spider man','thor','hulk','iron man','captain america']
+n=int(input("Enter a number"))
 
-heros.append("black panther")
-print(heros)
+arr=[]
+print(arr)
 
-heros.remove("black panther")
-print(heros)
-
-heros.insert(3,"black panther")
-print(heros)
-
-
-heros[1:3]=["Doctor Strange"]
-print(heros)
-
-heros.sort()
-print(heros)
+def odd():
+    for i in range(1,n+1):
+        if i%2!=0:
+            arr.append(i)
+           
+odd()
+print(arr)
