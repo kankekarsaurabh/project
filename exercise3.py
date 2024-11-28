8b5000f55136b4d6f8a96c93c5052050c2390c62
kankekarsaurabh <kankekarsaurabh98@gmail.com>
2024-11-27 23:39:02 -0800

    arrays

--- exercise3.py
+++ exercise3.py
@@ -141,28 +141,82 @@
 
 
 
-def validate(a,b,c):
-    sum=0
+# def validate(a,b,c):
+#     sum=0
 
-    if 1<=a<=11 and 1<=b<=11 and 1<=c<=11:
-        sum=a+b+c
-        if sum<=21:
-            return sum
-        elif sum >21 and (a==11 or b==11 or c==11):
-            sum=sum-10
-            if sum > 21:
-                return "BUST"
-            else:
-                return sum
-        elif sum>21:
-            return "BUST"
-    else:
-         return "Invalid input"
-try:    
-    num1=int(input("first number\n"))
-    num2=int(input("Second Number\n"))
-    num3=int(input("third number\n"))
-    result=validate(num1,num2,num3)
-    print(result)
-except ValueError:
-        print ("Invalid input")
+#     if 1<=a<=11 and 1<=b<=11 and 1<=c<=11:
+#         sum=a+b+c
+#         if sum<=21:
+#             return sum
+#         elif sum >21 and (a==11 or b==11 or c==11):
+#             sum=sum-10
+#             if sum > 21:
+#                 return "BUST"
+#             else:
+#                 return sum
+#         elif sum>21:
+#             return "BUST"
+#     else:
+#          return "Invalid input"
+# try:    
+#     num1=int(input("first number\n"))
+#     num2=int(input("Second Number\n"))
+#     num3=int(input("third number\n"))
+#     result=validate(num1,num2,num3)
+#     print(result)
+# except ValueError:
+#         print ("Invalid input")
+
+
+# 7) SUMMER OF '69: Return the sum of the numbers in the array,
+#  except ignore sections of numbers starting with a 6 and extending to the next 9 
+# (every 6 will be followed by at least one 9). Return 0 for no numbers.
+
+# summer_69([1, 3, 5]) --> 9
+# summer_69([4, 5, 6, 7, 8, 9]) --> 9
+# summer_69([2, 1, 6, 9, 11]) --> 14
+
+def six(a):
+    sum=0
+    for i in range(len(a)):
+        sum=i+sum
+        if a[i] == 6:
+            i+=1
+            if a[i]==9:
+                print(i)
+                return True
+
+array=[1,3,5]
+result= six(array)
+print(result)
+
+
+
+# 8) CHALLENGING PROBLEMS
+# SPY GAME: Write a function that takes in a list of integers and returns True if it contains 007 in order
+
+#  spy_game([1,2,4,0,0,7,5]) --> True
+#  spy_game([1,0,2,4,0,5,7]) --> True
+#  spy_game([1,7,2,0,4,5,0]) --> False
+
+
+
+# 9) COUNT PRIMES: Write a function that returns the number of prime numbers that exist up to and including a given number
+
+# count_primes(100) --> 25
+# By convention, 0 and 1 are not prime.
+
+
+# 10) Just for fun:
+
+# PRINT BIG: Write a function that takes in a single letter, and returns a 5x5 representation of that letter
+
+# print_big('a')
+
+# out:   *  
+#       * *
+#      *****
+#      *   *
+#      *   *
+# HINT: Consider making a dictionary of possible patterns, and mapping the alphabet to specific 5-line combinations of patterns.
+# For purposes of this exercise, it's ok if your dictionary stops at "E
